b16d1924693e9c71acb51ef86a7cbc7252ed9526
Shyam Bhat <shyambhat@me.com>
2014-02-02 04:10:28 +0530

    Error message

--- InstagramKit/Engine/InstagramEngine.h
+++ InstagramKit/Engine/InstagramEngine.h
@@ -22,7 +22,7 @@
 #import "AFNetworking.h"
 #import <CoreLocation/CoreLocation.h>
 
-//#error Insert your Instagram App Credentials Here
+#error Insert your Instagram App Credentials Here
 // Head over to http://instagram.com/developer/clients/manage/ to find these.
 
 #define APP_CLIENT_ID @"<Client_ID_here>"
