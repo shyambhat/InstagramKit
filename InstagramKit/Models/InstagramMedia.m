371faff92fcb0b6489c3c52200593963620ef377
Ryan <ryantan-93@live.com>
2015-02-22 17:56:09 +0800

    Added userHasLiked property to InstagramMedia.

--- InstagramKit/Models/InstagramMedia.m
+++ InstagramKit/Models/InstagramMedia.m
@@ -40,6 +40,7 @@
     if (self && IKNotNull(info)) {
         
         _user = [[InstagramUser alloc] initWithInfo:info[kUser]];
+        _userHasLiked = [info[kUserHasLiked] boolValue];
         _createdDate = [[NSDate alloc] initWithTimeIntervalSince1970:[info[kCreatedDate] doubleValue]];
         _link = [[NSString alloc] initWithString:info[kLink]];
         _caption = [[InstagramComment alloc] initWithInfo:info[kCaption]];
