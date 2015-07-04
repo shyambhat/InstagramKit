f258f841d0cb734c1d1d8ab914cd019715567f39
Shyam Bhat <shyambhat@me.com>
2015-07-04 20:10:10 +0200

    New Example Project

---
+++ InstagramKit-Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InstagramKit-Example
+//
+//  Created by Shyam Bhat on 04/07/15.
+//  Copyright (c) 2015 Shyam Bhat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
