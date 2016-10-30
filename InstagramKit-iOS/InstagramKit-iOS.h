e9ede0a1b01587f9a888ab04baf27342c9cb90d9
fliptoo <fonghuangyee@gmail.com>
2016-10-30 14:52:53 +0800

    - support framework

---
+++ InstagramKit-iOS/InstagramKit-iOS.h
@@ -0,0 +1,19 @@
+//
+//  InstagramKit-iOS.h
+//  InstagramKit-iOS
+//
+//  Created by fliptoo on 30/10/2016.
+//  Copyright © 2016 InstagramKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InstagramKit-iOS.
+FOUNDATION_EXPORT double InstagramKit_iOSVersionNumber;
+
+//! Project version string for InstagramKit-iOS.
+FOUNDATION_EXPORT const unsigned char InstagramKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstagramKit_iOS/PublicHeader.h>
+
+
