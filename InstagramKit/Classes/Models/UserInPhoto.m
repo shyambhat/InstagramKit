d46f1bb1286d7bfdcd52ec081c216ba34d5d937c
Shyam Bhat <shyambhat@me.com>
2018-06-04 04:28:55 +0200

    Copyright + license header

--- InstagramKit/Classes/Models/UserInPhoto.m
+++ InstagramKit/Classes/Models/UserInPhoto.m
@@ -1,5 +1,5 @@
 //
-//    Copyright (c) 2015 Shyam Bhat
+//    Copyright (c) 2018 Shyam Bhat
 //
 //    Permission is hereby granted, free of charge, to any person obtaining a copy of
 //    this software and associated documentation files (the "Software"), to deal in
